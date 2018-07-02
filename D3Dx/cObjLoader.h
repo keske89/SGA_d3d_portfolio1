0c315983fa9d8906bc2d217f13d0f67bb8b52848
log901108 <35515018+log901108@users.noreply.github.com>
2018-07-02 17:25:44 +0900

    objloader upload
    ObjLoader upload

---
+++ D3Dx/cObjLoader.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "stdafx.h"
+
+class cObjLoader
+{
+private:
+	CString m_filePath;
+	//map<CString, MTLTEX*> m_mtltexList;
+
+public:
+	cObjLoader();
+	~cObjLoader();
+
+	LPD3DXMESH LoadMesh(LPCTSTR filepath, LPCTSTR filename, D3DXMATRIX* pMat);
+
+};
+
