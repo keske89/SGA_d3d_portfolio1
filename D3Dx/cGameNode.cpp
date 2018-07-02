b89866de85fdfef9fd306e1bbe2ed5d03f8e66c4
samusaran <neitt@naver.com>
2018-07-02 18:51:43 +0900

    reset

---
+++ D3Dx/cGameNode.cpp
@@ -0,0 +1,34 @@
+#include "stdafx.h"
+#include "cGameNode.h"
+
+
+cGameNode::cGameNode()
+{
+}
+
+
+cGameNode::~cGameNode()
+{
+}
+
+void cGameNode::Setup()
+{
+}
+
+void cGameNode::Update()
+{
+}
+
+void cGameNode::Render()
+{
+}
+
+void cGameNode::Destroy()
+{
+}
+
+LRESULT cGameNode::WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
+{
+
+	return DefWindowProc(hWnd, iMessage, wParam, lParam);
+}
---
+++ D3Dx/cMainGame.cpp
@@ -0,0 +1,83 @@
+#include "stdafx.h"
+#include "cMainGame.h"
+#include "cCamera.h"
+
+
+
+cMainGame::cMainGame()
+	: m_pCamera(NULL)
+
+{
+}
+
+
+cMainGame::~cMainGame()
+{
+	delete m_pCamera;
+
+	TIMEMANAGER->Destroy();
+	SCENEMANAGER->Destroy();
+	g_pDeviceManager->Destroy();
+}
+
+void cMainGame::Setup()
+{
+	SetLight();
+	g_pD3DDevice->SetRenderState(D3DRS_LIGHTING, true);
+
+	
+
+
+	m_pCamera = new cCamera;
+	m_pCamera->Setup();
+
+
+}
+
+void cMainGame::Update()
+{
+	
+}
+
+void cMainGame::Render()
+{
+	g_pD3DDevice->Clear(NULL, NULL, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER,
+		D3DCOLOR_XRGB(0, 0, 255), 1.0f, 0);
+
+	g_pD3DDevice->BeginScene();
+
+	
+
+
+	g_pD3DDevice->EndScene();
+
+	g_pD3DDevice->Present(NULL, NULL, NULL, NULL);
+}
+
+void cMainGame::WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	if (m_pCamera)
+		m_pCamera->WndProc(hWnd, message, wParam, lParam);
+
+	
+}
+
+void cMainGame::SetLight()
+{
+	D3DLIGHT9	stLight;
+	ZeroMemory(&stLight, sizeof(D3DLIGHT9));
+
+	stLight.Type = D3DLIGHT_DIRECTIONAL;
+	stLight.Ambient = D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f);
+	stLight.Diffuse = D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f);
+	stLight.Specular = D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f);
+
+	D3DXVECTOR3 vDir(1.0f, -1.0f, 1.0f);
+	D3DXVec3Normalize(&vDir, &vDir);
+	stLight.Direction = vDir;
+	g_pD3DDevice->SetLight(0, &stLight);
+
+	g_pD3DDevice->LightEnable(0, true);
+}
+
+
