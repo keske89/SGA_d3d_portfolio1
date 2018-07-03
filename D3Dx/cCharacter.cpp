086f470fa3f93368b31ff086a71bfc9f017c9017
unknown <zxcszx5@naver.com>
2018-07-03 14:00:45 +0900

    캐릭터 오타수정
    오타수정

---
+++ D3Dx/cCharacter.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "cCharacter.h"
+
+
+cCharacter::cCharacter()
+{
+}
+
+
+cCharacter::~cCharacter()
+{
+}
---
+++ D3Dx/cCharacter.h
@@ -0,0 +1,8 @@
+#pragma once
+class cCharacter
+{
+public:
+	cCharacter();
+	~cCharacter();
+};
+
