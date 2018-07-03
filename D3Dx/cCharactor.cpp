58075da7e07cedfaff22e5d60d47e4a3116aaf94
unknown <zxcszx5@naver.com>
2018-07-03 11:50:43 +0900

    기본 캐릭터
    컷밋

---
+++ D3Dx/cCharactor.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "cCharactor.h"
+
+
+cCharactor::cCharactor()
+{
+}
+
+
+cCharactor::~cCharactor()
+{
+}
---
+++ D3Dx/cCharactor.h
@@ -0,0 +1,8 @@
+#pragma once
+class cCharactor
+{
+public:
+	cCharactor();
+	~cCharactor();
+};
+
